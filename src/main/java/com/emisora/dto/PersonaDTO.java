ee703e1a7c1bcbcfa229d10198f897b3b8120f5c
RojasD13 <daniel.rojas13@uptc.edu.co>
2025-05-03 19:43:05 -0500

    feature: Persona y sus roles
    Incluye: Controles, DTO, excepciones, repository y service

---
+++ src/main/java/com/emisora/dto/PersonaDTO.java
@@ -0,0 +1,24 @@
+package com.emisora.dto;
+
+import java.util.List;
+
+public class PersonaDTO {
+    private String nombre;
+    private String correo;
+    private List<RolDTO> roles;
+
+    public PersonaDTO() {}
+
+    public PersonaDTO(String nombre, String correo, List<RolDTO> roles) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.roles = roles;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+    public List<RolDTO> getRoles() { return roles; }
+    public void setRoles(List<RolDTO> roles) { this.roles = roles; }
+}
---
+++ src/main/java/com/emisora/repository/PersonaRepository.java
@@ -0,0 +1,6 @@
+package com.emisora.repository;
+
+import com.emisora.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonaRepository extends JpaRepository<Persona, Long> {}
