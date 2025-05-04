9b9e02283e9073dc658ca3fc426c83e84788486a
RojasD13 <daniel.rojas13@uptc.edu.co>
2025-05-04 17:50:38 -0500

    fix: import constraints

--- src/main/java/com/emisora/agenda/model/Persona.java
+++ src/main/java/com/emisora/agenda/model/Persona.java
@@ -1,6 +1,10 @@
 package com.emisora.agenda.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 @Entity
