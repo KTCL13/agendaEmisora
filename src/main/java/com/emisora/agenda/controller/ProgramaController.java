12bcef0dc78704c62cfe9da3d2a4a2d6b0884a68
KTCL13 <cesard20101@gmail.com>
2025-05-05 11:27:40 -0500

    Fix typo in savePrograma method and organize imports in ProgramaController

--- src/main/java/com/emisora/agenda/controller/ProgramaController.java
+++ src/main/java/com/emisora/agenda/controller/ProgramaController.java
@@ -1,8 +1,11 @@
 package com.emisora.agenda.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +29,7 @@ public class ProgramaController {
 
     @PostMapping("/addPrograma")
     public ResponseEntity<ProgramaDTO> savePrograma(@RequestBody ProgramaDTO programaDTO) {
-        ProgramaDTO ProgramaAdded = programaService.saveProgram(programaDTO);
+        ProgramaDTO ProgramaAdded = programaService.savePrograma(programaDTO);
         return new ResponseEntity<>(ProgramaAdded, HttpStatus.CREATED);
     }
 
