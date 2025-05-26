d1f65d5d92e745df269ec23aef65fdd88232a114
RojasD13 <daniel.rojas13@uptc.edu.co>
2025-05-25 20:29:07 -0500

    feature: generación de repote de canciones por programa utilizando Strategy

---
+++ src/main/java/com/emisora/agenda/reports/GeneradorDeReportes.java
@@ -0,0 +1,22 @@
+package com.emisora.agenda.reports;
+
+import com.emisora.agenda.dto.ReporteDTO;
+
+import java.util.Map;
+
+public class GeneradorDeReportes {
+
+    private EstrategiaDeReporte estrategiaActual;
+
+    public GeneradorDeReportes(EstrategiaDeReporte estrategiaInicial) {
+        this.estrategiaActual = estrategiaInicial;
+    }
+
+    public void setEstrategia(EstrategiaDeReporte nuevaEstrategia) {
+        this.estrategiaActual = nuevaEstrategia;
+    }
+
+    public ReporteDTO generarReporte(Map<String, Object> parametros) {
+        return estrategiaActual.generar(parametros);
+    }
+}
